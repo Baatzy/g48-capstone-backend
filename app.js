f26dc0837e125dcdc3ddf65cfff4d36f54dd793a
STEFAN BAATZ <waltz.for.zizi@gmail.com>
2017-07-11 11:19:27 -0700

    Changed view engine to ejs just in case, added some model and controller structure

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var profiles = require('./routes/profiles');
-var workouts = require('./routes/workouts');
-var logbooks = require('./routes/logbooks');
-var usersWorkouts = require('./routes/users_workouts');
-var usersUsers = require('./routes/users_users');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+// const methodOverride = require('method-override');
 
-var app = express();
 
-// view engine setup
+const index = require('./routes/index');
+// const users = require('./routes/users');
+// const profiles = require('./routes/profiles');
+// const workouts = require('./routes/workouts');
+// const logbooks = require('./routes/logbooks');
+// const usersWorkouts = require('./routes/users_workouts');
+// const usersUsers = require('./routes/users_users');
+
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,13 +28,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/', users);
-app.use('/', profiles);
-app.use('/', workouts);
-app.use('/', logbooks);
-app.use('/', users_workouts);
-app.use('/', users_users);
-
+// app.use('/', users);
+// app.use('/', profiles);
+// app.use('/', workouts);
+// app.use('/', logbooks);
+// app.use('/', users_workouts);
+// app.use('/', users_users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
